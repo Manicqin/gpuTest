8153a69dad8d1de373a993c20b19ed6c20cc1a45
Fishman Boaz <manicqin@gmail.com>
2020-04-21 22:28:09 +0300

    Update constants.py

--- notifications/disco_events/core/constants.py
+++ notifications/disco_events/core/constants.py
@@ -3,7 +3,7 @@ Constants and Enum used by the events sdk
 """
 import enum
 
-HOST_URL = 'host.docker.internal:8080'
+HOST_URL = 'http://host.docker.internal:8080'
 EVENTS_ENDPOINT = HOST_URL + '/update'
 
 class MessageType(enum.Enum):
