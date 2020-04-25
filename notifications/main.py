efc593a4266ed2af82cc8888686203a9f3ea2f05
Fishman Boaz <manicqin@gmail.com>
2020-04-25 22:09:47 +0300

    Update main.py

--- notifications/main.py
+++ notifications/main.py
@@ -1,15 +1,18 @@
 import disco_events
 import time
+try:
+    disco_events.setJobInfo('jobId', 'dispatchId')
 
-disco_events.setJobInfo('jobId', 'dispatchId')
-
-for i in range(1000):
+    for i in range(1000):
     
-    disco_events.ProgressMesasge("Running", i).send()
-    print(i)
-    time.sleep(1)
-    if i % 10 is 0:
-        disco_events.EventMesasge("Getting there").send()
-        print('event')
+        disco_events.ProgressMesasge("Running", i).send()
+        print(i)
+        time.sleep(1)
+        if i % 10 is 0:
+            disco_events.EventMesasge("Getting there").send()
+            print('event')
 
-disco_events.ProgressMesasge("Finished", 100).send()
+    disco_events.ProgressMesasge("Finished", 100).send()
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    time.sleep(60)
