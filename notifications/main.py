df33f16a2a958b9c27ae371799243ca40cd01c41
Fishman Boaz <manicqin@gmail.com>
2020-04-27 00:52:51 +0300

    Update main.py

--- notifications/main.py
+++ notifications/main.py
@@ -9,7 +9,7 @@ try:
     
         disco_events.ProgressMesasge("Running", i).send()
         print(i)
-        time.sleep(2)
+        time.sleep(1)
         if i % 10 is 0:
             disco_events.EventMesasge("Getting there").send()
             print('event')
