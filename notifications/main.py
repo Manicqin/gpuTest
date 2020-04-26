8f2e489b83a78169f4f4cfe806c191b2bcc39243
Fishman Boaz <manicqin@gmail.com>
2020-04-27 00:36:20 +0300

    Update main.py

--- notifications/main.py
+++ notifications/main.py
@@ -5,11 +5,11 @@ time.sleep(60)
 try:
     disco_events.setJobInfo('jobId', 'dispatchId')
 
-    for i in range(1000):
+    for i in range(100):
     
         disco_events.ProgressMesasge("Running", i).send()
         print(i)
-        time.sleep(1)
+        time.sleep(2)
         if i % 10 is 0:
             disco_events.EventMesasge("Getting there").send()
             print('event')
